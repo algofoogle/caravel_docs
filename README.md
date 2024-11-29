ebfd8f4fe9fc56bd41f096e35c3bed4da783c629
Anton Maurovic <anton@foogle.com>
2024-11-30 06:58:31 +1030

    Doing basic styling

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# Documentation source for Caravel frame and SoC
+# Documentation source for Caravel Frame and SoC
+
+This is the Sphinx source code for new Caravel "readthedocs"-style documentation intended to replace (or at least supersede) https://caravel-mgmt-soc-litex.readthedocs.io and https://caravel-harness.readthedocs.io
+
+## Decisions made
+
+*   Continue using Sphinx as the documentation compiler, for compatibility with existing docs.
+*   Retain the [`docs/`](./docs/) subdirectory so this can be planted into an existing repo (say, [caravel]) later.
+*   Switch to the [Wagtail theme](https://sphinx-wagtail-theme.readthedocs.io/) because it looks nice, supports full-width layouts and mobile views, and renders complex tables more sensibly.
+
+
+## Setting up
+
+```bash
+python3 -m venv --prompt caravel_docs .venv
+source .venv/bin/activate
+pip install -r docs/requirements.txt
+```
+
+
+## Building and viewing the documentation
+
+> [!NOTE]
+> I built this using Sphinx 7.3.7 which is the default that pip installed.
+
+First:
+
+```bash
+source .venv/bin/activate
+cd docs
+```
+
+Then for each build:
+
+```bash
+make html  # Updates docs/build/html/
+```
+
+And to view it (maybe persisting in a separate terminal):
+
+```bash
+python3 -m http.server --directory build/html
+```
+
+...and open http://localhost:8000/ in a web browser.
+
+
+## Editing
+
+I use VSCode. I have these extensions installed (but not sure whether I actually *benefit* from all or not):
+*   [reStructuredText][rst] (`lextudio.restructuredtext`)
+*   [reStructuredText Syntax highlighting][rstsh] (`trond-snekvik.simple-rst`)
+*   [Esbonio][esbonio] (`swyddfa.esbonio`) -- I'm not sold on this one yet. It always complains about the server not working. Not sure what it's actually for, but I think it's bundled with the "reStructuredText" extension above anyway, and it's maybe for analyzing your content/doctree to provide easier cross-linking and intellisense...?
+
+
+## Customization
+
+[`docs/source/conf.py`](./docs/source/conf.py) contains Sphinx configuration for how this documentation should be built, and its appearance.
+
+
+## readthedocs
+
+[readthedocs.io](https://readthedocs.io) is a service which automates the building and hosting of documentation, typically using Sphinx. It can watch pushes to this repo and automatically handle building/deployment. It relies on [`.readthedocs.yaml`](./.readthedocs.yaml)
+
+
+[caravel]: https://github.com/efabless/caravel
+[rst]: https://marketplace.visualstudio.com/items?itemName=lextudio.restructuredtext
+[rstsh]: https://marketplace.visualstudio.com/items?itemName=trond-snekvik.simple-rst
+[esbonio]: https://marketplace.visualstudio.com/items?itemName=swyddfa.esbonio
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,22 @@
-# Configuration file for the Sphinx documentation builder.
+# SPDX-FileCopyrightText: 2024 Efabless Corporation
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+
+# Configuration file for the Sphinx documentation builder.
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -13,15 +30,68 @@ author = 'Efabless'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+  'sphinxcontrib.wavedrom', # For rendering register diagrams.
+  'sphinx.ext.mathjax',
+  'sphinx.ext.todo',
+  'sphinx_wagtail_theme'
+]
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = []
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = [
+    'build',
+    'Thumbs.db',
+    '.DS_Store',
+    '.gitignore',
+    'requirements.txt',
+    '.keep',
+    # Files included in other rst files.
+    'introduction.rst', # Included in index.rst
+]
+
+master_doc = "index"
+today_fmt = "%Y-%m-%d %H:%M"
+todo_include_todos = False
+# numfig = True
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_wagtail_theme'
+html_show_sphinx = False # Hide footer Sphinx/Wagtail links; formatting is broken.
+html_last_updated_fmt = today_fmt
+html_favicon = 'i/Efabless-globe-favicon-192.png'
 html_static_path = ['_static']
+html_extra_path = [
+    '_static/efabless-logo-dark-bg.png', # Force Efabless logo build, so Wagtail theme can use it.
+]
+
+# These are options specifically for the Wagtail Theme.
+html_theme_options = dict(
+    project_name = "Caravel SoC and Harness",
+    logo = "efabless-logo-dark-bg.png", # I've found this needs to be in html_extra_path.
+    logo_alt = "Efabless",
+    logo_url = "/",
+    logo_width = 125,
+    logo_height = 30,
+    github_url = "https://github.com/efabless/caravel_docs/blob/main/docs/",
+    header_links = "Efabless.com|https://efabless.com, GitHub/efabless|https://github.com/efabless", # In top-right corner of header bar
+    footer_links = ",".join([
+        "Efabless.com|https://efabless.com",
+        "Efabless Knowledgebase|https://info.efabless.com/knowledge-base",
+        "caravel_user_project|https://github.com/efabless/caravel_user_project",
+    ]),
+)
+
+html_sidebars = {"**": [
+    "searchbox.html",
+    "globaltoc.html",
+    "menu_footer.html",    # Extra HTML in menu_footer.html displays at bottom of sidebar menu.
+]}
+
